20a3dafd7ba2d4223393263e024ca62a7c2098ae
Rupam Jana <rupam.jana@oraczen.ai>
2025-12-23 19:08:50 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+// Font configuration using Next.js Google Fonts
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
